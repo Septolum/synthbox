dc84f38467cc3c6851287e5817836dc9ae7e4115
Septolum <septolum@gmail.com>
2019-01-27 16:47:10 +0000

    Added background check to support midi bank patch changes, and restructured for multi channel control

--- synthbox.py
+++ synthbox.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 import fluidsynth, time
 
-channel = 0
+currChannel = 0
 currSF2Path = "/home/pi/GeneralUser GS 1.47/GeneralUser GS v1.47.sf2"
 currPatchName = ""
 currBank = 0
 currPatch = 0
 bankpatchlist = []
+inMenu = False
 
 ### SF2 Handling Setup ###
 from sf2utils.sf2parse import Sf2File
@@ -21,6 +22,23 @@ def getSF2bankpatchlist(sf2path: str):
 
 	return([[int(i[0]), int(i[1])] for i in [i.split(":") for i in sorted([i[7:14] for i in str(sf2.presets)[1:-1].split(", ")])[:-1]]])
 
+def switchSF2(sf2path:str, channel: int, bank: int, patch: int):
+	'''
+	Changes the current soundfont, patch, and bank for a given channel, and changes the current values to represent that.
+	'''
+	global currChannel
+	global sfid
+	global bankpatchlist
+	global currPatchName
+	global currBank
+	global currPatch
+	sfid = fs.sfload(sf2path)
+	bankpatchlist = getSF2bankpatchlist(sf2path)
+	currChannel = channel
+	currBank = bank
+	currPatch = patch
+	currPatchName = fs.channel_info(currChannel)[3].decode("utf-8")
+	fs.program_select(currChannel, sfid, currBank, currPatch)
 
 ### SF2 Handling Setup End ####
 
@@ -33,10 +51,13 @@ fs.setting('audio.realtime-prio', 99)
 
 fs.start(driver='alsa', device='hw:Device,0', midi_driver='alsa_seq')
 
-sfid = fs.sfload(currSF2Path)
-fs.program_select(0, sfid, 0, 0)	
+#sfid = fs.sfload(currSF2Path)
+switchSF2(currSF2Path, 9, 128, 0)
+#fs.program_select(0, sfid, 0, 0)
+switchSF2(currSF2Path, 0, 0, 0)
+#fs.program_select(9, sfid, 128, 0)
 
-def patchInc(channel: int, bankpatchlist):
+def patchInc():
 	'''
 	Finds next non empty patch, moving to the next bank if needs be.
 	Max bank 128 before it loops around to 0.
@@ -44,8 +65,8 @@ def patchInc(channel: int, bankpatchlist):
 	global currPatchName
 	global currBank
 	global currPatch
-	currBank = fs.channel_info(channel)[1]
-	currPatch = fs.channel_info(channel)[2]
+	currBank = fs.channel_info(currChannel)[1]
+	currPatch = fs.channel_info(currChannel)[2]
 	currIndex = bankpatchlist.index([currBank, currPatch])
 
 	if (currIndex + 1) == len(bankpatchlist):
@@ -53,11 +74,11 @@ def patchInc(channel: int, bankpatchlist):
 	else:
 		currIndex += 1
 	[currBank, currPatch] = bankpatchlist[currIndex]
-	fs.program_select(channel, sfid, currBank, currPatch)
-	currPatchName = fs.channel_info(channel)[3].decode("utf-8")
-	print(fs.channel_info(channel))
+	fs.program_select(currChannel, sfid, currBank, currPatch)
+	currPatchName = fs.channel_info(currChannel)[3].decode("utf-8")
+	print(fs.channel_info(currChannel))
 
-def patchDec(channel: int, bankpatchlist):
+def patchDec():
 	'''
 	Finds previous non empty patch, moving to the previous bank if needs be.
 	Max bank 128 after looping around from 0.
@@ -65,8 +86,8 @@ def patchDec(channel: int, bankpatchlist):
 	global currPatchName
 	global currBank
 	global currPatch
-	currBank = fs.channel_info(channel)[1]
-	currPatch = fs.channel_info(channel)[2]
+	currBank = fs.channel_info(currChannel)[1]
+	currPatch = fs.channel_info(currChannel)[2]
 	currIndex = bankpatchlist.index([currBank, currPatch])
 
 	if (currIndex - 1) == -1:
@@ -74,9 +95,9 @@ def patchDec(channel: int, bankpatchlist):
 	else:
 		currIndex -= 1
 	[currBank, currPatch] = bankpatchlist[currIndex]
-	fs.program_select(channel, sfid, currBank, currPatch)
-	currPatchName = fs.channel_info(channel)[3].decode("utf-8")
-	print(fs.channel_info(channel))
+	fs.program_select(currChannel, sfid, currBank, currPatch)
+	currPatchName = fs.channel_info(currChannel)[3].decode("utf-8")
+	print(fs.channel_info(currChannel))
 
 ### End FluidSynth Setup ###
 
@@ -89,8 +110,8 @@ lcd = CharLCD(pin_rs=22, pin_e=23, pins_data=[9, 25, 11, 8], cols=16, rows=2, nu
 
 def writeLCD(firstline: str, secondline: str):
 	'''
-	Writes 2 lines to a 16x2 LCD
-	Shortens them by removing vowels if needed
+	Writes 2 lines to a 16x2 LCD.
+	Shortens them by removing vowels if needed.
 	'''
 	lcd.clear()
 	if len(firstline) > 16:
@@ -112,13 +133,13 @@ def writeLCD(firstline: str, secondline: str):
 from pyky040 import pyky040
 
 def my_inccallback(scale_position):
-	if scale_position%2 == 0:
-		patchInc(channel, bankpatchlist)
+	if scale_position%2 == 0: # Trigger every 2 'rotations' as my rotary encoder sends 2 per 1 physical click
+		patchInc()
 		writeLCD(currPatchName, 'Bank ' + str(currBank) + ' Patch ' + str(currPatch))
 
 def my_deccallback(scale_position):
 	if scale_position%2 == 0:
-		patchDec(channel, bankpatchlist)
+		patchDec()
 		writeLCD(currPatchName, 'Bank ' + str(currBank) + ' Patch ' + str(currPatch))
 
 def my_swcallback():
@@ -133,9 +154,37 @@ my_encoder.setup(scale_min=1, scale_max=100, step=1, loop=True, inc_callback=my_
 
 ### End Rotary Encoder Setup ###
 
-bankpatchlist = getSF2bankpatchlist(currSF2Path)
+### Background Bank & Patch Setup ###
+# This is needed because midi devices can request bank and patch changes themselves, and it's probably easier doing this than intercepting the raw midi calls and handling them oursleves
+import threading
 
-currPatchName = fs.channel_info(channel)[3].decode("utf-8")
+def bgBankPatchCheck():
+	'''
+	Checks if the bank and/or patch has changed in the background without us noticing.
+	'''
+	global currPatchName
+	global currBank
+	global currPatch
+
+	while True:
+		if ( (currBank != fs.channel_info(currChannel)[1]) | (currPatch != fs.channel_info(currChannel)[2]) ):
+			currBank = fs.channel_info(currChannel)[1]
+			currPatch = fs.channel_info(currChannel)[2]
+			currPatchName = fs.channel_info(currChannel)[3].decode("utf-8")
+			if not inMenu:
+				# change the text too
+				writeLCD(currPatchName, 'Bank ' + str(currBank) + ' Patch ' + str(currPatch))
+		time.sleep(0.1)
+
+bg_thread = threading.Thread(target=bgBankPatchCheck)
+bg_thread.start()
+
+### End Background Bank & Patch Setup ###
+
+
+#bankpatchlist = getSF2bankpatchlist(currSF2Path)
+
+currPatchName = fs.channel_info(currChannel)[3].decode("utf-8")
 writeLCD(currPatchName, 'Bank ' + str(currBank) + ' Patch ' + str(currPatch))
 
 my_encoder.watch()
